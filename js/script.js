dc86d727edced6cded11c9ed8b7267195d464ac0
Vladislav Gernet <skeimorr@gmail.com>
2023-12-06 12:27:24 +0400

    removing symbols upgrade

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ import {initialPageCondition} from './initial-page-condition.js';
 
 initialPageCondition();
 initTabs();
-// const replaceNumber = (string, position, newValue) => string.slice(0, position) + newValue + string.slice(position + 1);
+const replaceNumber = (string, position) => string.slice(0, position) + string.slice(position + 1);
 const backSpaceButton = 8;
 const numbersArray = ['0','1','2','3','4','5','6','8','9'];
 const restrictedSymbolsToRemove = ['(', ')', '-'];
 const phoneField = document.querySelector('#phone-pick-up');
-const getFieldNumbersValue = (input) => input.value.replace(/\D/g, '');
+const getFieldNumbersValue = (value) => value.replace(/\D/g, '');
 const onPhoneFieldInput = (evt) => {
   const input = evt.target;
   const selectionStart = input.selectionStart;
   if (selectionStart !== input.value.length) {
     return;
   }
-  const inputValue = getFieldNumbersValue(input);
+  const inputValue = getFieldNumbersValue(input.value);
   let formattedValue = '';
   if (inputValue[0] === '7') {
     formattedValue += '+7';
@@ -36,6 +36,7 @@ const onPhoneFieldInput = (evt) => {
     formattedValue += `-${inputValue.substring(9, 11)}`;
   }
   input.value = formattedValue;
+  input.setSelectionRange(2,3);
 };
 //KeyDownCheck
 const checkAllowedKeys = (evt) => {
@@ -71,26 +72,43 @@ const onPhoneFieldKeydown = (evt) => {
   //check symbol to remove
   const symbolToRemove = input.value[selectionStart - 1];
   if (evt.keyCode === backSpaceButton) {
-    if (selectionStart < 4) {
-      input.value = '';
+    if (input.value.length > 4 && selectionStart < 4) {
+      evt.preventDefault();
       return;
     }
     if (restrictedSymbolsToRemove.indexOf(symbolToRemove) > -1) {
       evt.preventDefault();
       input.setSelectionRange(selectionStart - 1, selectionStart - 1);
     }
+    const newValue = replaceNumber(input.value, selectionStart - 1);
+    const inputValue = getFieldNumbersValue(newValue);
+    let formattedValue = '';
+    if (inputValue[0] === '7') {
+      formattedValue += '+7';
+    }
+    if (inputValue.length > 1) {
+      formattedValue += `(${inputValue.substring(1, 4)}`;
+    }
+    if (inputValue.length > 4) {
+      formattedValue += `)${inputValue.substring(4, 7)}`;
+    }
+    if (inputValue.length > 7) {
+      formattedValue += `-${inputValue.substring(7, 9)}`;
+    }
+    if (inputValue.length > 9) {
+      formattedValue += `-${inputValue.substring(9, 11)}`;
+    }
+    evt.preventDefault();
+    input.value = formattedValue;
+    //Перемещает курсор на 1 символ влево
+    input.setSelectionRange(selectionStart - 1, selectionStart - 1);
+    /*
+    остановился на удалении символов, удалении перед +7, и на добавлении символов от середины.
+     */
   }
-  // console.log('stop')
-  // evt.preventDefault();
-  // const isKeyNumberDown = /\d/.test(evt.key);
-  // if (isKeyNumberDown) {
-  //   const newValue = replaceNumber(input.value, symbolToRemove, evt.key);
-  // }
 };
 phoneField.addEventListener('input', onPhoneFieldInput);
 phoneField.addEventListener('keydown', onPhoneFieldKeydown);
-
-
 
 
 
