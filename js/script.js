7e277d48fef43c0b2c5345686064e4a85c45a5fb
Vladislav Gernet <skeimorr@gmail.com>
2023-12-07 21:42:17 +0400

    delete button check added

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ const checkAllowedKeys = (evt) => {
   const isShortcutHighlight = evt.ctrlKey && evt.keyCode === 65;
   const isShortcutInsert = evt.ctrlKey && evt.keyCode === 86;
   const isKeyNumberDown = /\d/.test(evt.key);
-  const isBackSpaceButton = evt.key === 'Backspace';
+  const isBackspaceButton = evt.key === 'Backspace';
+  const isDeleteButton = evt.key === 'Delete';
   const isArrowRight = evt.key === 'ArrowRight';
   const isArrowLeft = evt.key === 'ArrowLeft';
   const keysArray = [
@@ -52,7 +53,8 @@ const checkAllowedKeys = (evt) => {
     isShortcutHighlight,
     isShortcutInsert,
     isKeyNumberDown,
-    isBackSpaceButton,
+    isBackspaceButton,
+    isDeleteButton,
     isArrowRight,
     isArrowLeft
   ];
@@ -69,7 +71,32 @@ const onPhoneFieldKeydown = (evt) => {
   const selectionStart = input.selectionStart;
   //check symbol to remove
   const symbolToRemove = input.value[selectionStart - 1];
-  if (evt.key === 'Backspace') {
+  if (/\d/.test(evt.key)) {
+    //Остановился на добавлении символов от середины
+    // const inputValue = getFieldNumbersValue(input.value);
+    // let formattedValue = '';
+    // if (inputValue[0] === '7') {
+    //   formattedValue += '+7(';
+    // }
+    // if (numbersArray.indexOf(inputValue[0]) > -1) {
+    //   formattedValue = '';
+    //   formattedValue += `+7(${inputValue[0]}`;
+    // }
+    // if (inputValue.length > 1) {
+    //   formattedValue += `${inputValue.substring(1, 4)}`;
+    // }
+    // if (inputValue.length > 4) {
+    //   formattedValue += `)${inputValue.substring(4, 7)}`;
+    // }
+    // if (inputValue.length > 7) {
+    //   formattedValue += `-${inputValue.substring(7, 9)}`;
+    // }
+    // if (inputValue.length > 9) {
+    //   formattedValue += `-${inputValue.substring(9, 11)}`;
+    // }
+    // input.value = formattedValue;
+  }
+  if (evt.key === 'Backspace' || evt.key === 'Delete') {
     if (input.value.length >= 4 && selectionStart <= 3) {
       evt.preventDefault();
       return;
@@ -108,66 +135,3 @@ const onPhoneFieldKeydown = (evt) => {
 };
 phoneField.addEventListener('input', onPhoneFieldInput);
 phoneField.addEventListener('keydown', onPhoneFieldKeydown);
-
-
-
-
-
-
-
-
-
-
-
-// const firstRussianPhoneNumbers = ['7','8','9'];
-// const phoneField = document.querySelector('#phone');
-// const getInputNumbersValue = (input) => input.value.replace(/\D/g, '');
-// const onPhoneFieldInput = (evt) => {
-//   const input = evt.target;
-//   let inputNumbersValue = getInputNumbersValue(input);
-//   let formattedInputValue = '';
-//   const selectionStart = input.selectionStart;
-//   if (input.value.length !== selectionStart) {
-//     if (evt.data && /\D/g.test(evt.data)) {
-//       input.value = inputNumbersValue;
-//     }
-//     return;
-//   }
-//
-//   if (firstRussianPhoneNumbers.indexOf(inputNumbersValue[0]) > -1) {
-//     //russian phone number
-//     if (inputNumbersValue[0] === '9') {
-//       inputNumbersValue = `7${inputNumbersValue}`;
-//     }
-//     const firstSymbols = (inputNumbersValue[0] === '8') ? '8' : '+7';
-//     formattedInputValue = `${firstSymbols}`;
-//     if (inputNumbersValue.length > 1) {
-//       formattedInputValue += `(${inputNumbersValue.substring(1, 4)}`;
-//     }
-//     if (inputNumbersValue.length >= 5) {
-//       formattedInputValue += `)${inputNumbersValue.substring(4, 7)}`;
-//     }
-//     if (inputNumbersValue.length >= 8) {
-//       formattedInputValue += `-${inputNumbersValue.substring(7, 9)}`;
-//     }
-//     if (inputNumbersValue.length >= 10) {
-//       formattedInputValue += `-${inputNumbersValue.substring(9, 11)}`;
-//     }
-//   } else {
-//     // not Russian number
-//     formattedInputValue = `+${inputNumbersValue.substring(0, 16)}`;
-//   }
-//   input.value = formattedInputValue;
-// };
-// const onPhoneFieldKeydown = (evt) => {
-//   const input = evt.target;
-//   if (evt.keyCode === 8 && getInputNumbersValue(input).length === 1) {
-//     evt.target.value = '';
-//   } else if (getInputNumbersValue(input).length === 0) {
-//     evt.target.value = '';
-//   }
-// };
-//
-// phoneField.addEventListener('input', onPhoneFieldInput);
-// phoneField.addEventListener('keydown', onPhoneFieldKeydown);
-
