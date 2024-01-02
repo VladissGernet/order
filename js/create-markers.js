57654533ece52cbc7d2380adcb4e81f42f069c70
Vladislav Gernet <skeimorr@gmail.com>
2024-01-02 22:21:59 +0400

    address on click getter

--- js/create-markers.js
+++ js/create-markers.js
@@ -1,10 +1,25 @@
 import {icon} from './constants.js';
+import {addressContainer} from './elements.js';
 
 const createMarkers = (points, layer) => {
   for (const point of points) {
     const lat = point.coordinates[0];
     const lng = point.coordinates[1];
-    const marker = L.marker({lat, lng}, {icon});
+    const marker = L.marker(
+      {
+        lat,
+        lng
+      },
+      {
+        icon,
+        title: point.address,
+      });
+    marker.on('click', () => {
+      console.log(point.address);
+      console.log(addressContainer.querySelector('input[value="point.address"]'))
+      //остановился на создании переключателя адреса по клику на карте
+      //сейчас просто выводится в консоль адрес точки по которой кликнули
+    });
     marker.addTo(layer);
   }
 };
